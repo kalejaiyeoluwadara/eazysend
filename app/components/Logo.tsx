a761b010fcadcf0e75160d9812f4638c3a5b78c1
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-03-04 11:51:09 +0100

    updated link

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 function Logo() {
     return (
         <div className="flex gap-3">
-            <Link href={"https://outlook.office365.com/mail/"}>
+            <Link target='_blank' href={"https://outlook.office365.com/mail/"}>
                 <Image alt="outlook" className="h-8 w-8" src={outlook} />
             </Link>
             <h1 className="text-2xl font-bold mb-8">EazySend</h1>
